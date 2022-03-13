fcd86bc58a68bbae35c964200a850307c2078e0a
rahim <rahim.suleymanov94@gmail.com>
2022-03-13 11:45:41 +0400

    alias-lar quruldu

---
+++ alias.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+doskey pa=php artisan
+
+doskey pas              = php artisan serve
+doskey pamig            = php artisan migrate
+doskey pamigref         = php artisan migrate:refresh
+doskey pamigrefseed     = php artisan migrate:refresh --seed
